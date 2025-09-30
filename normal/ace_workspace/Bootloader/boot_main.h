17a7db293af599ad913e56871baf0a12ebcb5747
nguyen-thanh-tung <nguyenthanhtung8196@gmail.com>
2025-10-01 08:20:59 +0900

    add bootloader for stm32f407

---
+++ normal/ace_workspace/Bootloader/boot_main.h
@@ -0,0 +1,24 @@
+/**
+ * @file       boot_main.h
+ * @brief      
+ * @date       2025/09/30
+ * @author     [Gentantun] (nguyenthanhtung8196@gmail.com)
+ * @details    
+ * @ref        
+ * @copyright  Copyright (c) 2025 RoboTun
+*/
+
+#ifndef _BOOT_MAIN_H_
+#define _BOOT_MAIN_H_
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* CODE */
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
